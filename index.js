2d4af8a0ecf3779fa8b00cc1fc17c6dba53a5292
sampod76 <sampodnath76@gmail.com>
2022-09-03 03:17:36 +0600

    add alll loadData function

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 // data load 
 
-const loadDatas = async () => {
+const loadDatas = async (catagorisID) => {
 
-    const respons = await fetch('https://openapi.programming-hero.com/api/news/category/01')
+    const respons = await fetch(`https://openapi.programming-hero.com/api/news/category/${catagorisID}`)
     const datas = await respons.json()
     displayNews(datas.data)
 }
 
 const displayNews = (allNews) => {
     const getOutputFuild = document.getElementById("display-fuild")
-
+    getOutputFuild.innerHTML = ''
+    arrayValue = allNews.length
+   
+    const getElementById=document.getElementById("no-found")
+    if (arrayValue === 0) {
+    getElementById.classList.remove("d-none")
+    }
+    else{
+        getElementById.classList.add("d-none")
+    }
     for (const news of allNews) {
         // console.log(news)
-        const { rating, title, author, thumbnail_url, image_url, details,_id:id } = news
+        const { rating, title, author, thumbnail_url, image_url, details, _id: id } = news
         const { name, published_date, img } = author
         const { number: ratingNumber } = rating
-
+      
         const createElement = document.createElement("div")
-        
+
         createElement.classList.add("col")
         createElement.innerHTML = `
         <div class="card mb-3">
@@ -28,8 +37,8 @@ const displayNews = (allNews) => {
     <div class="col-md-8">
       <div class="card-body">
         <h5 class="card-title">${title}</h5>
-        <p class="card-text">${details.slice(0,300)}</p>
-        <p class="card-text">${details.slice(300,1000)}</p>
+        <p class="card-text">${details.slice(0, 300)}</p>
+        <p class="card-text">${details.slice(300, 1000)}</p>
         <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
       </div>
       <!-- img -->
@@ -39,7 +48,7 @@ const displayNews = (allNews) => {
                    <img style="width:3rem" class="rounded-circle" src="${img}" alt="">
                </div>
              <div class="ms-2">
-              <p>${name}</p>
+              <p>${name?name:"No found"}</p>
               <p>${published_date}</p>
              </div>
            </div> 
@@ -56,33 +65,56 @@ const displayNews = (allNews) => {
 </div>
        
     `
-    getOutputFuild.appendChild(createElement)
+        getOutputFuild.appendChild(createElement)
 
     }
 
 }
 
-const toggleBtn =async(newsId)=>{
-   const respons =await fetch(`https://openapi.programming-hero.com/api/news/${newsId}`)
-   const datas =await respons.json()
-   console.log(datas.data[0])
-   displayModal(datas.data[0])
+const toggleBtn = async (newsId) => {
+    const respons = await fetch(`https://openapi.programming-hero.com/api/news/${newsId}`)
+    const datas = await respons.json()
+    console.log(datas.data[0])
+    displayModal(datas.data[0])
 }
 
- const displayModal=(modalDetails)=>{
+const displayModal = (modalDetails) => {
 
-  const getModalBody=document.getElementById("modal-body")
- const {total_view,author,image_url}=modalDetails
- const {name,published_date,img}=author
- getModalBody.innerHTML=`
+    const getModalBody = document.getElementById("modal-body")
+    const { total_view, author, image_url } = modalDetails
+    const { name, published_date, img } = author
+    getModalBody.innerHTML = `
  <img class="img-fluid" src="${image_url}" alt="">
  <img style="width:3rem" class="rounded-circle mt-2" src="${img}" alt="">
- <h5>Total view :${total_view ?total_view:"No found"}</h5>
- <h5>Author: ${name}</h5>
+ <h5>Total view :${total_view ? total_view : "No found"}</h5>
+ <h5>Author: ${name?name:"no found"}</h5>
  <h5>Published Date :${published_date}</h5>
  
  `
 
- }
+}
 
-loadDatas()
+
+const catagoriFunction = async () => {
+
+    const respons = await fetch(`https://openapi.programming-hero.com/api/news/categories`)
+    const catagorisDatas = await respons.json()
+    displayCatagori(catagorisDatas.data.news_category)
+}
+
+const displayCatagori = (catagorisLink) => {
+    const getCatagoriFuild = document.getElementById("catagoriId")
+
+    catagorisLink.forEach(catagori => {
+        const { category_id, category_name } = catagori
+        const createElement = document.createElement("a")
+        createElement.innerHTML = `
+        <button class="rounded border border-success p-2 bg-info text-white" onclick="loadDatas('${category_id}')">${category_name}</button>
+        `
+        getCatagoriFuild.appendChild(createElement)
+
+    });
+}
+catagoriFunction()
+
+loadDatas("01")
